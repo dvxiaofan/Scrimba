22d379b7c27ad9a26b54dc0c3b6f9aba06695f38
xiaofan <1353815633@qq.com>
2019-03-28 17:12:15 +0800

    changes styles ways

--- react-one/src/index.js
+++ react-one/src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
+// import App from './components/App';
+
+
+function App() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+    const styles = {
+        backgroundColor: '#ddd'
+    }
+
+    if (hours < 12) {
+        timeOfDay = "morning";
+        styles.color = '#fff';
+    }
+    else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon";
+        styles.color = '#ff0';
+    }
+    else {
+        timeOfDay = "night";
+        styles.color = '#00f';
+    }
+
+    return (
+        <h1 style={styles}>Good {timeOfDay}</h1>
+    )
+}
 
 
 
